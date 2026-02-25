e45f3e848a090c070d922ba9afbd620e804ff247
Deepak Soni <deep.x.lis@gmail.com>
2026-02-26 00:00:27 +0530

    Case Changes

--- frontend/src/components/DashHeader.jsx
+++ frontend/src/components/DashHeader.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../styles/header.css'
+import '../styles/Header.css'
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import API from "../api";
